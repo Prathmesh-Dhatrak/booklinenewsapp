b3a0f41aae15f5c9fc8137989d63c6e95d823f3c
Prathmesh Santosh Dhatrak <prathmesh101dhatrak@gmail.com>
2022-12-21 20:42:58 +0530

    v2 setup

---
+++ app/redux/store.js
@@ -0,0 +1,12 @@
+import {
+  legacy_createStore as createStore,
+  applyMiddleware,
+  compose,
+} from "redux";
+import thunk from "redux-thunk";
+
+import reducers from "./rootReducer"; //Import the root reducer
+
+const enhancer = compose(applyMiddleware(thunk));
+
+export default createStore(reducers, enhancer);
